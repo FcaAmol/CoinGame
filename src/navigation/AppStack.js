8245f8059be57fe08c80182d6596495619945c78
Amol Tandon <amol.tandon@mail.valuecoders.com>
2024-01-25 16:15:05 +0530

    updates

---
+++ src/navigation/AppStack.js
@@ -0,0 +1,11 @@
+import Login from "../screens/Login";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// const Tab = createMaterialBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const AppStack = () => {
+  return <View></View>;
+};
+
+export default AppStack;
